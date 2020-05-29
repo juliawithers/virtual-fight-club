4c7502303e4a0aba5cb6edfa31f3df428dca4913
Julia Fehr <09jwithers@gmail.com>
2020-05-28 22:41:44 -0400

    updating endpoints

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export default {
-    API_LOGIN_ENDPOINT: `https://glacial-escarpment-11177.herokuapp.com/api/vfc/login`,
-    API_USERS_ENDPOINT: `https://glacial-escarpment-11177.herokuapp.com/api/vfc/users`,
-    API_CHARACTERS_ENDPOINT: `https://glacial-escarpment-11177.herokuapp.com/api/vfc/characters`,
-    API_CHARACTERS_ID_ENDPOINT: `https://glacial-escarpment-11177.herokuapp.com/api/vfc/characters/:id`,
-    API_MATCHES_ENDPOINT: `https://glacial-escarpment-11177.herokuapp.com/api/vfc/matches`,
+    API_LOGIN_ENDPOINT: `https://virtual-fight-club.herokuapp.com/api/vfc/login`,
+    API_USERS_ENDPOINT: `https://virtual-fight-club.herokuapp.com/api/vfc/users`,
+    API_CHARACTERS_ENDPOINT: `https://virtual-fight-club.herokuapp.com/api/vfc/characters`,
+    API_CHARACTERS_ID_ENDPOINT: `https://virtual-fight-club.herokuapp.com/api/vfc/characters/:id`,
+    API_MATCHES_ENDPOINT: `https://virtual-fight-club.herokuapp.com/api/vfc/matches`,
     // API_KEY: process.env.API_KEY,
     API_KEY: '0810bd3e-6112-4c27-a63f-c533e885495c'
   }
