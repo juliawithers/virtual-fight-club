efcad2318fc9f62d9ed896dec805f52db149e711
Julia Fehr <09jwithers@gmail.com>
2020-05-30 10:27:21 -0400

    updating befor initialy deploy

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe(`App component`, () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>,
+            div
+        );
+        ReactDOM.unmountComponentAtNode(div);
+    })
+    it('renders the UI as expected',() =>{
+        const wrapper = shallow(<App/>)
+        expect(toJson(wrapper)).toMatchSnapshot();
+    })
+})
