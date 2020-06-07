2518f6877dd40344e2b4de1aea54729a75c052bf
Julia Fehr <09jwithers@gmail.com>
2020-06-07 01:40:08 -0400

    updated readme

--- src/AboutGame/AboutGame.js
+++ src/AboutGame/AboutGame.js
@@ -14,12 +14,10 @@ export default class AboutGame extends Component {
                     <p>In Virtual Fight Club you can create a character and fight opponents! Get to the next level by gaining points from wins. 50 points to reach each new level. Once you reach the next level you can update the character attributes of your choosing. Strength, Intelligence, Charisma, and Agility are the attributes you must work with. Each character is unique! </p>
                     
                     <p>And remember, the FIRST RULE OF FIGHT CLUB: You do not talk about (virtual) Fight Club!</p>
-                    <p>[placeholder for screenshot of character page]</p>
                 </section>
                 <section>
                     <h2>The FIGHT</h2>
                     <p>Each opponent is chosen at random and the winner is determined by an algorithm that incorporates the attribute values and random chance. Win and gain points towards your next upgrade. Lose and you won't lose anything, you just won't gain any points.</p>
-                    <p>[placeholder for screenshot of fight page]</p>
                 </section>
             </div>
         )
